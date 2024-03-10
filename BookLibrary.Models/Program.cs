bfc19e3627e9c463f5dcb09d2278f6f7bcdd43c7
Rafael Brito <britorafael1995@gmail.com>
2024-03-10 17:04:26 -0300

    creation of BookLibrary.Models project

---
+++ BookLibrary.Models/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookLibrary.Models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
