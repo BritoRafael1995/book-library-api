60e7942775b425ec7ddd479a0000b51c680a5e79
Rafael Brito <britorafael1995@gmail.com>
2024-03-10 18:45:50 -0300

    configured autoMapper

---
+++ BookLibrary.Models/Dto/BookDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Models.Dto
+{
+    public class BookDto
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public int TotalCopies { get; set; }
+        public int CopiesInUse { get; set; }
+        public string Type { get; set; }
+        public string Isbn { get; set; }
+        public List<string> Categories { get; set; }
+
+    }
+}
---
+++ BookLibrary.Models/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BookLibrary.Models.Dto;
+using BookLibrary.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Models
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookDto>()
+                .ForMember(d => d.Type, src => src.MapFrom(m => m.BookType.Description))
+                .ForMember(d => d.Authors, src => src.MapFrom(m => MapAuthors(m.Authors)))
+                .ForMember(d => d.Categories, src => src.MapFrom(m => MapCategories(m.Categories)));
+        }
+
+        private List<string> MapCategories(List<BookCategory> categories)
+        {
+            return categories.Select(c => c.Description).ToList();
+        }
+
+        private List<string> MapAuthors(List<Author> authors)
+        {
+            return authors.Select(a => $"{a.FirstName} {a.LasName}").ToList();
+        }
+    }
+}
