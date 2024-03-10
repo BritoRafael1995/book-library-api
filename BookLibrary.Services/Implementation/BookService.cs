f76df580205b37d80d2879fb1fc8221e9daaf3cd
Rafael Brito <britorafael1995@gmail.com>
2024-03-10 19:42:16 -0300

    created search method on service

---
+++ BookLibrary.Services/Implementation/BookService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BookLibrary.DataStore.SQLServer.Interface;
+using BookLibrary.Models;
+using BookLibrary.Models.Dto;
+using BookLibrary.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Services.Implementation
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IMapper _mapper;
+
+        public BookService(IBookRepository bookRepository, IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<BookDto>> Search(SearchFilter filter)
+        {
+            var books = await _bookRepository.Search(filter);
+
+            return _mapper.Map<List<BookDto>>(books);
+        }
+    }
+}
---
+++ BookLibrary.Services/Interface/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using BookLibrary.Models;
+using BookLibrary.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Services.Interface
+{
+    public interface IBookService
+    {
+        Task<List<BookDto>> Search(SearchFilter filter);
+    }
+}
