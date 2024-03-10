2cd81b2ac81a71141c93122dff5f6eb8e876fbbd
Rafael Brito <britorafael1995@gmail.com>
2024-03-10 17:03:10 -0300

    creation of bookLibrary.Services project

---
+++ BookLibrary.Services/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BookLibrary.Services
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
